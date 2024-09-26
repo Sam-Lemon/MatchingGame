0da8090334e193a252cce3e73977bd317825b8c4
Sam-Lemon <theatrewhisper@gmail.com>
2024-09-26 16:47:54 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+4x4 gameboard 
+on click card flips over showing image 
+if both images match cards disappear, player gets point 
+if match more than one pair in a row, extra point 
+if doesn't match, both cards flip back over showing backside of card
+win when all cards are cleared
+
+
+
+
+
